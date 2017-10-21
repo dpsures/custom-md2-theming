0b2c351d954b8b93379c782ec3e107466b8cece2
Suresh Kumar Devaraj <dpsures@gmail.com>
2017-10-21 17:29:19 +0530

    included home & few routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,16 +3,25 @@ import { RouterModule, Routes } from "@angular/router";
 import { MaterialModule } from "./material.module";
 import { HeaderComponent } from "./header/header.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { AboutComponent } from "./about/about.component";
+import { HomeComponent } from "./home/home.component";
 
 const APP_ROUTES : Routes = [
-    {path:'', redirectTo:'/dashboard', pathMatch:'full'},
-    {path:'dashboard', component: DashboardComponent}
+    {path:'', redirectTo:'/trainers', pathMatch:'full'},
+    {path:'trainers', component: DashboardComponent,
+        children: [
+            {path:'', redirectTo:'home', pathMatch:'full'},
+            {path: 'home', component: HomeComponent},
+            {path:'about', component:AboutComponent}
+        ]
+    }
 ];
 
 @NgModule({
     imports: [MaterialModule, RouterModule.forRoot(APP_ROUTES)],
     exports: [RouterModule],
-    declarations: [DashboardComponent, HeaderComponent],
+    declarations: [DashboardComponent, HeaderComponent, AboutComponent,
+    HomeComponent],
     providers: [],
 })
 export class AppRoutingModule { }
